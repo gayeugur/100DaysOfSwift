cdfa755b6637465a274b0bce4ebac8f7cec1c570
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2024-01-04 10:02:12 +0300

    Add files via upload

---
+++ Project12/Project12a/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project12a
+//
+//  Created by gayeugur on 17.12.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ Project12/Project12aUITests/Project12aUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project12aUITestsLaunchTests.swift
+//  Project12aUITests
+//
+//  Created by gayeugur on 17.12.2023.
+//
+
+import XCTest
+
+final class Project12aUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

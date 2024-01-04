2762ac4e157ae2cfd8708843edbc80da2feee796
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2024-01-04 10:04:37 +0300

    Add files via upload

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by gayeugur on 18.12.2023.
+//
+
+import Foundation
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
